c447a02142ed4b293b53ffa32af2d1c9cac52840
MoneroLite <73478668+MoneroLite@users.noreply.github.com>
2023-01-02 19:38:56 +0500

    del and upd routes

--- front/src/routes/routes.ts
+++ front/src/routes/routes.ts
@@ -1,6 +1,6 @@
 import Chat from "../pages/Chat/Chat";
 import Main from "../pages/Main/Main";
-import { CHAT_ROUTE, MAIN_ROUTE } from "./consts";
+import { CHAT_ROUTE, MAIN_ROUTE } from "../utils/consts";
 
 export const publicRoutes = [
   {
