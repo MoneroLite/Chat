2aaebc7e76c9c2c5d49aca3d577e9eef68f98f6e
MoneroLite <73478668+MoneroLite@users.noreply.github.com>
2023-01-05 23:19:21 +0500

    upd: signin, chat

--- front/src/components/screens/signIn/SignIn.tsx
+++ front/src/components/screens/signIn/SignIn.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { SubmitButton } from "../../ui/buttons/main/SubmitButton";
 import { BasicInput } from "../../ui/inputs/main/BasicInput";
 import styles from "./signIn.module.scss";
 
 export const SignIn = () => {
+  const nav = useNavigate();
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const onClickJoin = () => {
+    nav(`/chat?name=${name}&room=${room}`);
+  };
   return (
     <div>
-      <BasicInput />
-      <BasicInput />
-      <SubmitButton>Войти</SubmitButton>
+      <BasicInput value={name} onChange={(e) => setName(e.target.value)} />
+      <BasicInput value={room} onChange={(e) => setRoom(e.target.value)} />
+      <SubmitButton onClick={onClickJoin}>Войти</SubmitButton>
     </div>
   );
 };
--- front/src/pages/Chat/Chat.tsx
+++ front/src/pages/Chat/Chat.tsx
@@ -1,7 +1,20 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import io from "socket.io-client";
 import { Layout } from "../../components/layout/Layout";
 
+const socket = io("http://localhost:5000");
+
 const Chat: FC = () => {
+  const { search } = useLocation();
+  const [params, setParams] = useState<null | {}>(null);
+  useEffect(() => {
+    const searchParams = Object.fromEntries(new URLSearchParams(search));
+    setParams(searchParams);
+
+    socket.emit("join", searchParams);
+  }, [search]);
+
   return <Layout>Chat</Layout>;
 };
 
